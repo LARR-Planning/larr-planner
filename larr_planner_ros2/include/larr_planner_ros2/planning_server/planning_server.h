18f9e700d75aebc81190005dd38c9d36fcc136df
yunwoo <YunwooLee94@github.com>
2024-05-19 11:37:32 +0900

    Add Control Publisher and Planning and Control Thread

--- larr_planner_ros2/include/larr_planner_ros2/planning_server/planning_server.h
+++ larr_planner_ros2/include/larr_planner_ros2/planning_server/planning_server.h
@@ -7,7 +7,9 @@
 
 // #include <pcl/PCLPointCloud2.h>
 // #include <pcl_conversions/pcl_conversions.h>
+#include <cmath>
 #include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/twist_stamped.hpp>
 #include <larr_planner/wrapper/Wrapper.h>
 #include <rclcpp/rclcpp.hpp>
 #include <visualization_msgs/msg/marker_array.hpp>
@@ -15,21 +17,31 @@
 using RobotStateMsg = geometry_msgs::msg::PoseStamped;
 using StateSubscriber = rclcpp::Subscription<RobotStateMsg>::SharedPtr;
 using RosTimer = rclcpp::TimerBase::SharedPtr;
-
+using TwsitControlInputMsg = geometry_msgs::msg::TwistStamped;
+using TwistInputPublisher = rclcpp::Publisher<TwsitControlInputMsg>::SharedPtr;
 namespace larr_planner {
 
 class PlanningServer : public rclcpp::Node {
 private:
   Wrapper *wrapper_ptr_;
+  // Subscriber
   StateSubscriber state_subscriber_;
 
+  // Subscriber callback
+  void RobotStateCallback(const RobotStateMsg::ConstSharedPtr &msg);
+  RobotState ConvertToRobotState(const RobotStateMsg &state_msg);
+
+  // Publisher
+  TwistInputPublisher twsit_input_publisher_;
+
+  // Timer
   RosTimer planning_timer_;
   RosTimer control_timer_;
+  // Timer Callback
   void PlanningTimerCallback();
   void ControlTimerCallback();
 
-  void RobotStateCallback(const RobotStateMsg::ConstSharedPtr &msg);
-  RobotState ConvertToRobotState(const RobotStateMsg &state_msg);
+  TwsitControlInputMsg ConvertToTwsitMsg(const LinearSystemControlInput &control_input);
 
 public:
   PlanningServer(const rclcpp::NodeOptions &options_input);
--- larr_planner_ros2/src/planning_server/planning_server.cc
+++ larr_planner_ros2/src/planning_server/planning_server.cc
@@ -14,6 +14,9 @@ PlanningServer::PlanningServer(const rclcpp::NodeOptions &options_input) : Node(
   using namespace std::chrono_literals;
   control_timer_ = this->create_wall_timer(20ms, std::bind(&PlanningServer::ControlTimerCallback, this));
   planning_timer_ = this->create_wall_timer(200ms, std::bind(&PlanningServer::PlanningTimerCallback, this));
+
+  // Publish
+  twsit_input_publisher_ = create_publisher<TwsitControlInputMsg>("twist_control_input", rclcpp::QoS(1));
 }
 
 void PlanningServer::RobotStateCallback(const geometry_msgs::msg::PoseStamped::ConstSharedPtr &msg) {
@@ -35,4 +38,29 @@ RobotState PlanningServer::ConvertToRobotState(const RobotStateMsg &state_msg) {
   return state;
 }
 void PlanningServer::PlanningTimerCallback() { wrapper_ptr_->OnPlanningTimerCallback(); }
-void PlanningServer::ControlTimerCallback() {}
+
+void PlanningServer::ControlTimerCallback() {
+  auto time = now();
+  auto linear_control_input = wrapper_ptr_->GenerateControlInputFromPlanning(time.seconds());
+  if (linear_control_input.has_value()) {
+    twsit_input_publisher_->publish(ConvertToTwsitMsg(linear_control_input.value()));
+  } else {
+    LinearSystemControlInput linear_control_input_at_fail;
+    linear_control_input_at_fail.t_sec = time.seconds();
+    linear_control_input_at_fail.vx = 0.0, linear_control_input_at_fail.vy = 0.0, linear_control_input_at_fail.vz = 0.0;
+    linear_control_input_at_fail.wx = 0.0, linear_control_input_at_fail.wy = 0.0, linear_control_input_at_fail.wz = 0.0;
+    twsit_input_publisher_->publish(ConvertToTwsitMsg(linear_control_input_at_fail));
+  }
+}
+
+TwsitControlInputMsg PlanningServer::ConvertToTwsitMsg(const LinearSystemControlInput &control_input) {
+  TwsitControlInputMsg msg;
+  control_input.t_sec;
+  msg.header.frame_id = "map";
+  msg.header.stamp.sec = floor(control_input.t_sec);
+  msg.header.stamp.nanosec = floor(1E9 * (control_input.t_sec - msg.header.stamp.sec));
+  msg.twist.linear.x = control_input.vx, msg.twist.linear.y = control_input.vy, msg.twist.linear.z = control_input.vz;
+  msg.twist.angular.x = control_input.wx, msg.twist.angular.y = control_input.wy,
+  msg.twist.angular.z = control_input.wz;
+  return msg;
+}
